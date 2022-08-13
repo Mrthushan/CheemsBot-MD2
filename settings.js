4275cd8b1d2a4af62c267e42ca5e90fe95ff51e1
Mrthushan <109566515+Mrthushan@users.noreply.github.com>
2022-08-13 15:22:23 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,10 +21,10 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['916909137213']
+global.owner = ['94771018371']
 global.premium = ['916909137213']
 global.ownernomer = '916909137213'
-global.ownername = '🦄Dream Guy Xeon'
+global.ownername = '🦄MR THUSHAN'
 global.botname = 'Cheems Bot MD V2'
 global.footer = 'Xeon Bot Inc.'
 global.ig = 'https://github.com/DGXeon'
